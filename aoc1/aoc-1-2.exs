964c9fe6d79d8b98fd9c28c620e39443be12e0c4
FraSanga <108059916+FraSanga@users.noreply.github.com>
2025-12-01 08:31:34 +0100

    clean up

--- aoc1/aoc-1-2.exs
+++ aoc1/aoc-1-2.exs
@@ -1,10 +1,10 @@
 defmodule Dial do
   def unlock("R"<>rest, pos) do
-    {number, rotations} = format_number(rest) |> dbg()
+    {number, rotations} = format_number(rest)
     {rem(pos + number, 100), rotations + div(pos + number - 1, 100)}
   end
   def unlock("L"<>rest, pos) do
-    {number, rotations} = format_number(rest) |> dbg()
+    {number, rotations} = format_number(rest)
     extra_rotation = if pos != 0, do: 1, else: 0
     if number > pos, do: {100 - (number - pos), rotations + extra_rotation}, else: {pos - number, rotations}
   end
@@ -22,6 +22,6 @@ File.stream!(file)
   {new_pos, rotations} = Dial.unlock(move, pos)
   {[{new_pos, rotations}], new_pos}
 end)
-|> Enum.to_list() |> dbg()
+|> Enum.to_list()
 |> Enum.reduce(0, fn {pos, rotations}, acc -> if pos == 0, do: rotations + acc + 1, else: rotations + acc end)
 |> then(fn code ->IO.puts("#{code}") end)
