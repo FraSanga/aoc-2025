5c9619e502a805a4f03d14b06f51d8b5bbc7a95f
FraSanga <108059916+FraSanga@users.noreply.github.com>
2025-12-02 16:27:03 +0100

    resolved day 1 part 1

--- aoc2/aoc-2.exs
+++ aoc2/aoc-2.exs
@@ -1,11 +1,11 @@
 defmodule IDsChecker do
   def check_range(range) do
-    [low_str, high_str] = String.split(range, "-", parts: 2) |> dbg()
+    [low_str, high_str] = String.split(range, "-", parts: 2)
     low_len = String.length(low_str)
     high_len = String.length(high_str)
 
-    if low_len == high_len, do: find_equal_start(low_str, high_str, ""), else: ""
-    |> generate_invalid_ids(low_str, high_str, low_len, high_len)
+    prefix = if low_len == high_len, do: find_equal_start(low_str, high_str, ""), else: ""
+    generate_invalid_ids(prefix, low_str, high_str, low_len, high_len)
     |> Enum.sum()
   end
 
@@ -28,24 +28,44 @@ defmodule IDsChecker do
     end
   end
   # Jump to the nearest lower bound with an even length
-  defp generate_invalid_ids("", low_str, high_str, low_len, high_len) when rem(low_len,2) == 1 do
+  defp generate_invalid_ids("", _, high_str, low_len, high_len) when rem(low_len,2) == 1 do
     new_low_str = Integer.to_string(10**low_len)
-    if low_len + 1 == high_len, do: find_equal_start(new_low_str, high_str, ""), else: ""
-    |> generate_invalid_ids(new_low_str, high_str, low_len + 1, high_len)
+    prefix = if low_len + 1 == high_len, do: find_equal_start(new_low_str, high_str, ""), else: ""
+    generate_invalid_ids(prefix, new_low_str, high_str, low_len + 1, high_len)
   end
   # Jump to the nearest upper bound with an even length
-  defp generate_invalid_ids("", low_str, high_str, low_len, high_len) when rem(high_len,2) == 1 do
+  defp generate_invalid_ids("", low_str, _, low_len, high_len) when rem(high_len,2) == 1 do
     new_high_str = Integer.to_string((10**(high_len-1))-1)
-    if low_len + 1 == high_len, do: find_equal_start(low_str, new_high_str, ""), else: ""
-    |> generate_invalid_ids(low_str, new_high_str, low_len, high_len - 1)
+    prefix = if low_len + 1 == high_len, do: find_equal_start(low_str, new_high_str, ""), else: ""
+    generate_invalid_ids(prefix, low_str, new_high_str, low_len, high_len - 1)
   end
 
   # Generate all possible ids with a fixed prefix
-  defp generate_invalid_ids(prefix_str, low_str, high_str, len, len) do 
-    prefix_len = String.length(prefix_str) 
+  defp generate_invalid_ids(prefix_str, low_str, high_str, len, len) do
+    prefix_len = String.length(prefix_str)
     prefix = String.to_integer(prefix_str)
-    generate_invalid_ids("", String.slice(low_str, prefix_len..-1), String.slice(high_str, prefix_len..-1), len-prefix_len, len-prefix_len) 
-    |> Enum.map(fn n -> n + (prefix * (10**(len-prefix_len))) end)
+    low = String.to_integer(low_str)
+    high = String.to_integer(high_str)
+    half_len = div(len,2)
+    cond do
+      prefix_len == half_len ->
+        number = prefix * (10**half_len + 1)
+        if number in low..high, do: [number], else: []
+      prefix_len > half_len ->
+        half = div(prefix,10**(prefix_len-half_len))
+        number = half * (10**half_len + 1)
+        if number in low..high, do: [number], else: []
+      prefix_len < half_len ->
+        dim = half_len - prefix_len
+        prefix_low = div(low,10**half_len) |> rem(10**dim)
+        prefix_high = div(high,10**half_len) |> rem(10**dim)
+        for i <- prefix_low..prefix_high,
+            n = (prefix * (10**(dim)) + i) * (10**half_len + 1),
+            n in low..high,
+            reduce: [] do
+          acc -> [n | acc]
+        end
+    end
   end
 end
 
@@ -54,6 +74,6 @@ file = "aoc-2-input.txt"
 File.stream!(file)
 |> Stream.map(&String.split(&1,",", trim: true))
 |> Enum.to_list()
-|> List.first() |> dbg()
+|> List.first()
 |> Enum.reduce(0, fn range, acc -> IDsChecker.check_range(range) + acc end)
 |> then(fn code ->IO.puts("#{code}") end)
